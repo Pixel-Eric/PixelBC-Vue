ec404b9ad5e04534d03f6baae67e36a95bdc4ebe
Eric <487737744@qq.com>
2022-01-06 13:59:53 +0800

    development

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { getToken } from '../session' 
 
+
 const app = {
     path:'/',
     name:'app',
@@ -41,6 +42,20 @@ const router = createRouter({
   routes
 })
 
+//重写VueRouter的push方法
+//防止跳转当前相同路由时会报错
+
+const originPush = router.options.push;
+const originReplace = router.options.replace;
+
+router.options.push = function(location,resolve,reject){
+    if(resolve&&reject){
+        originPush.call(this,location,resolve,reject);
+    }else{
+        originPush.call(this,location,()=>{},()=>{});
+    }
+}
+
 router.beforeEach((to,from,next)=>{
     if(to.name == 'login'){
         if(getToken()!=null){
@@ -49,6 +64,6 @@ router.beforeEach((to,from,next)=>{
     }
     next();
 });
-   
+router.
 
 export default router
